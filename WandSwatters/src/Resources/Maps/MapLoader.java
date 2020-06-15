f2dd0ef5e1ae0dc0839e27fb0cd6c3823f1d35f8
Dawson Gilmore <dfgilmor@lakeheadu.ca>
2020-06-15 18:08:16 -0400

    NOT FUNCTIONAL, DO NOT USE, ITS JUST BEING PUSHED SO ITS HERE IF MY COMPUTER DIES FOREVER

--- WandSwatters/src/Resources/Maps/MapLoader.java
+++ WandSwatters/src/Resources/Maps/MapLoader.java
@@ -8,9 +8,13 @@ public class MapLoader {
     
     Net m0n0;
     Net m0n1;
+    Net m0n2;
+    Net m0n3;
     Net background;
     Wall m0w0;
     Wall m0w1;
+    Wall m0w2;
+    Wall m0w3;
     Wall _background;
     Map m0;
     public MapLoader(){
@@ -18,10 +22,16 @@ public class MapLoader {
     
         background = new Net(new Coord[]{new Coord(0,0), new Coord(0,720), new Coord(1000,720), new Coord(1000,0)});
         m0n0 = new Net(new Coord[]{new Coord(200,200), new Coord(200,300), new Coord(300,300), new Coord(300,200)});
+        m0n1 = new Net(new Coord[]{new Coord(600,600), new Coord(600,700), new Coord(700,700), new Coord(700,600), new Coord(650, 600), new Coord(650,625), new Coord(675, 625), new Coord(675, 675), new Coord(625, 675), new Coord(625, 625), new Coord(650, 625), new Coord(650, 600)});
+        m0n2 = new Net(new Coord[]{new Coord(200,350), new Coord(200,450), new Coord(300,450), new Coord(300,350)});
+        m0n3 = new Net(new Coord[]{new Coord(350,200), new Coord(350,450), new Coord(450,450), new Coord(450,200)});
         
-        m0w0 = new Wall(m0n0, Color.RED, 0);
+        m0w0 = new Wall(m0n0, Color.RED, 1);
+        m0w1 = new Wall(m0n1, Color.RED, 0);
+        m0w2 = new Wall(m0n2, Color.RED, 1);
+        m0w3 = new Wall(m0n3, Color.RED, 1);
         _background = new Wall(background, Color.WHITE, 1);
-        m0 = new Map("Map 0", /*_background,*/ m0w0 /*whats this?*/);
+        m0 = new Map("Map 0", _background, m0w0 /*whats this?*/,m0w1, m0w2, m0w3);
 
         mapList.add(m0);
     }
